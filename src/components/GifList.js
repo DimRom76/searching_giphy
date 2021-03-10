5dfc2ab7224772415513018762a3000920aa639e
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-03-10 13:16:37 +0200

    find gif

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GifItem from "./GifItem";
 
-const GifList = (props) => {
+const GifList = function (props) {
   const gifItems = props.gifs.map((image) => {
-    return <GifItem key={image.id} gif={image} />;
+    return (
+      <GifItem key={image.id} gif={image} onGifSelect={props.onGifSelect} />
+    );
   });
 
-  return <ul>{gifItems}</ul>;
+  return <ul className="gif-list">{gifItems}</ul>;
 };
 
 export default GifList;
