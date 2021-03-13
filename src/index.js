c1c5e113ffbc042de3a6d7d3ed922375f30a1eb3
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-03-14 00:17:26 +0200

    Routing and Auth

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 import { Provider } from "react-redux";
-import configureStore from "./store/configureStore";
+import { configureStore } from "./store/configureStore";
+import { BrowserRouter } from "react-router-dom";
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("app")
 );
